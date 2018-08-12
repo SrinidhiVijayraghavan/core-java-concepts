02c92ba02596c20f33394ebc8d226905291ffa0b
Srinidhi <40656364+SrinidhiVijayraghavan@users.noreply.github.com>
2018-08-13 00:48:38 +0530

    Add files via upload

---
+++ addclass.java
@@ -0,0 +1,17 @@
+package addition;
+
+public class addclass {
+	void calculate()
+	{
+		
+		System.out.println(10+20);
+	}
+		public static void main(String[] args) {
+			
+	addclass r1 =new addclass();
+	r1.calculate();
+		}
+
+	}
+
+
